2069407a6114e89e517ff8edc2ee002452ddb5bb
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-08-26 20:35:33 +0900

    initial commit

---
+++ lib/ex_bdf_parser.ex
@@ -0,0 +1,18 @@
+defmodule ExBdfParser do
+  @moduledoc """
+  Documentation for ExBdfParser.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBdfParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_bdf_parser_test.exs
@@ -0,0 +1,8 @@
+defmodule ExBdfParserTest do
+  use ExUnit.Case
+  doctest ExBdfParser
+
+  test "greets the world" do
+    assert ExBdfParser.hello() == :world
+  end
+end
