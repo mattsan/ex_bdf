8d9316551940a2fb5d7555af5648fc654c35d6c9
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-08-27 22:39:06 +0900

    show bitmap image

--- lib/ex_bdf_parser.ex
+++ lib/ex_bdf_parser.ex
@@ -8,4 +8,19 @@ defmodule ExBDFParser do
   def load(filename, conversion \\ nil) do
     File.open(filename, &Parser.parse(&1, conversion))
   end
+
+  def show_bitmap_image(fonts, code) do
+    fonts[code]
+    |> IO.inspect()
+
+    fonts[code].bitmap
+    |> Enum.each(fn bitmap ->
+      bitmap
+      |> Integer.to_string(2)
+      |> String.pad_leading(16, "0")
+      |> String.replace("0", "  ")
+      |> String.replace("1", "[]")
+      |> IO.puts()
+    end)
+  end
 end
