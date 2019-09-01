ab9f9236f70472737aae32dbf3ea1e7e2d3cac0a
Eiji MATSUMOTO <e.mattsan@gmail.com>
2019-09-01 22:28:20 +0900

    render a string into a bitmap

--- lib/ex_bdf/font.ex
+++ lib/ex_bdf/font.ex
@@ -31,7 +31,7 @@ defmodule ExBDF.Font do
     File.open(filename, &Parser.parse(&1, opts))
   end
 
-  def bitmap(%Font{} = font, foreground \\ <<1::1>>, background \\ <<0::1>>) do
+  def bitmap(%Font{} = font, foreground \\ <<1::1>>, background \\ <<0::1>>) when is_binary(foreground) and is_binary(background) do
     width = font.width
 
     font.bitmap
@@ -40,6 +40,47 @@ defmodule ExBDF.Font do
         case bit do
           0 -> background
           1 -> foreground
+        end
+      end
+    end)
+  end
+
+  def string_to_bitmap(str, foreground \\ <<1::1>>, background \\ <<0::1>>) when is_binary(str) and is_binary(foreground) and is_binary(background) do
+    fonts =
+      str
+      |> String.to_charlist()
+      |> Enum.map(&ExBDF.get_font/1)
+      |> Enum.filter(&(not is_nil(&1)))
+
+    {bottom, top} =
+      fonts
+      |> Enum.reduce({0, 0}, fn font, {bottom, top} ->
+        {
+          Enum.min([bottom, font.bbx.offset_y]),
+          Enum.max([top, font.bbx.height + font.bbx.offset_y - 1])
+        }
+      end)
+
+    fonts
+    |> Enum.map(fn font ->
+      width = font.width
+      padding_bottom = font.bbx.offset_y - bottom
+      padding_top = top - (font.bbx.height + font.bbx.offset_y - 1)
+
+      List.duplicate(0, padding_top) ++ font.bitmap ++ List.duplicate(0, padding_bottom)
+      |> Enum.map(&<<&1::size(width)>>)
+    end)
+    |> Enum.zip()
+    |> Enum.map(fn row ->
+      line =
+        row
+        |> Tuple.to_list()
+        |> Enum.into("")
+
+      for <<bit::1 <- line >>, into: "" do
+        case bit do
+          1 -> foreground
+          0 -> background
         end
       end
     end)
